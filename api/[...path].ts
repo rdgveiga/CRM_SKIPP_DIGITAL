0bf00cb03fc5ecdb3c2206ccd6fdf4360b40dd20
rdgoveiga <rdgo.veiga@gmail.com>
2026-09-11 19:07:29 -0300

    Expose Meta API routes on Vercel

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,6 +1,4 @@
 // Catch-all serverless function da Vercel.
 // A Vercel roteia automaticamente TODO request sob /api/* para este arquivo,
 // repassando o caminho completo ao app Express (rotas /api/...).
-import { app } from '../backend/src/app.js';
-
-export default app;
+export { default } from './_app.js';
